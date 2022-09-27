24dcfd01299b5d8086b02f69e9b4a467f395e611
Phu Vuong <pvuong@vuonglab.com>
2022-09-26 21:27:03 -0700

    No need to load Material Icons font from CDN.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,6 @@ export const Head = () =>
       rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
     />
-    <link
-      id="mui-font-icons"
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
   </>
 
 export default function Home() {
