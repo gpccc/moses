b141497447f3ed59bc5e280960e5687542ab0086
Phu Vuong <pvuong@vuonglab.com>
2022-09-26 21:23:27 -0700

    Add app bar.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,63 @@
 import React from "react"
-import Button from "@mui/material/Button"
+
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+import AdbIcon from "@mui/icons-material/Church"
 
 export const Head = () =>
   <>
-  <link
-    id="mui-roboto-font"
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-  />
-  <link
-    id="mui-font-icons"
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-  />
+    <link
+      id="mui-roboto-font"
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+    <link
+      id="mui-font-icons"
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
   </>
 
 export default function Home() {
-  return <Button variant="contained">Hello world!</Button>
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Greater Phoenix Chinese Christian Church
+          </Typography>
+
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            GPCCC
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
 }
