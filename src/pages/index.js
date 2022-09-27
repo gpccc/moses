b53cf7af025b4333c160bd97724ecb839d5da936
Phu Vuong <pvuong@vuonglab.com>
2022-09-26 19:15:12 -0700

    Load Roboto font and font icons via Google CDN.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
 
+export const Head = () =>
+  <>
+  <link
+    id="mui-roboto-font"
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  />
+  <link
+    id="mui-font-icons"
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
+  </>
+
 export default function Home() {
   return <div>Hello world!</div>
 }
