1f6f7b05e70286ec1016f3456a9c0caf481e3974
Phu Vuong <pvuong@vuonglab.com>
2022-09-27 22:37:28 -0700

    Change home page to use page layout.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,17 @@
 import React from "react"
-
-import AppBar from "@mui/material/AppBar"
-import Toolbar from "@mui/material/Toolbar"
+import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
-import Container from "@mui/material/Container"
-import ChurchIcon from "@mui/icons-material/Church"
 
-export const Head = () =>
-  <>
-    <link
-      id="mui-roboto-font"
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    />
-  </>
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <ChurchIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Greater Phoenix Chinese Christian Church
-          </Typography>
-
-          <ChurchIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            GPCCC
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  )
+    <Layout>
+      <Box>
+        <Typography variant="h2" gutterBottom>
+          Welcome, newcomer!
+        </Typography>
+      </Box>
+    </Layout>
+ )
 }
