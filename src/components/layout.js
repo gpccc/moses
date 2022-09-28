3a1bce7ec7efcfedeb75aadc0f8ef507213fc44f
Phu Vuong <pvuong@vuonglab.com>
2022-09-27 22:33:43 -0700

    Add layout with app bar.

---
+++ src/components/layout.js
@@ -0,0 +1,59 @@
+import React from "react"
+
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+import ChurchIcon from "@mui/icons-material/Church"
+
+export const Head = () =>
+    <>
+    <link
+        id="mui-roboto-font"
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+    </>
+
+export default ({ children }) => (
+    <>
+    <AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+                <ChurchIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                <Typography
+                    variant="h5"
+                    component="a"
+                    href="/"
+                    sx={{
+                        mr: 2,
+                        display: { xs: 'none', md: 'flex' },
+                        fontWeight: 700,
+                        color: 'inherit',
+                        textDecoration: 'none',
+                    }}
+                >
+                    Greater Phoenix Chinese Christian Church
+                </Typography>
+
+                <ChurchIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                <Typography
+                    variant="h6"
+                    component="a"
+                    href="/"
+                    sx={{
+                        mr: 2,
+                        display: { xs: 'flex', md: 'none' },
+                        fontWeight: 700,
+                        color: 'inherit',
+                        textDecoration: 'none',
+                    }}
+                >
+                    GPCCC
+                </Typography>
+            </Toolbar>
+        </Container>
+    </AppBar>
+    {children}
+    </>
+)
