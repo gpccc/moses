24e987b0fceaa11594abfbe98ac004bb661640a6
Phu Vuong <pvuong@vuonglab.com>
2023-07-14 21:29:31 -0700

    Add online giving button to app bar.

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import React from "react"
 import CssBaseline from "@mui/material/CssBaseline"
 
 import AppBar from "@mui/material/AppBar"
+import Button from '@mui/material/Button'
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
 import Container from "@mui/material/Container"
@@ -18,10 +19,11 @@ export const Head = () =>
     />
     </>
 
+const onlineGivingLink = "http://gpccc.org/2020/05/12/support-gpccc-online-giving-%e6%94%af%e6%8c%81%e5%87%a4%e5%9f%8e%e6%95%99%e4%bc%9a-%e7%88%b1%e5%bf%83%e5%a5%89%e7%8c%ae/"
 const Layout = ({ children }) => (
     <>
     <CssBaseline />
-    <AppBar position="static">
+    <AppBar position="static" color="secondary">
         <Container maxWidth="xl">
             <Toolbar disableGutters>
                 <ChurchIcon sx={{ mr: 1 }} />
@@ -34,10 +36,12 @@ const Layout = ({ children }) => (
                         fontWeight: 700,
                         color: 'inherit',
                         textDecoration: 'none',
+                        flexGrow: 1,
                     }}
                 >
                     GPCCC
                 </Typography>
+                <Button href={onlineGivingLink} target="_blank" variant="contained" size="large" color="primary">Online Giving</Button>
             </Toolbar>
         </Container>
     </AppBar>
