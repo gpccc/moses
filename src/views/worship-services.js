b3345529c2cec922c9a876d227bb6912e9ba8627
Phu Vuong <pvuong@vuonglab.com>
2023-07-16 00:20:00 -0700

    Show worship service card.

--- src/views/worship-services.js
+++ src/views/worship-services.js
@@ -1,9 +1,21 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 
+import ServiceCard from '../components/services/service-card'
+
+import englishServices from '../constants/english-services'
+import mandarinServices from '../constants/mandarin-services'
+import cantoneseServices from '../constants/cantonese-services';
+
 const WorshipServices = () => (
     <Container component="section" sx={{ my: 4 }}>
-      <h1>Worship Services</h1>
+      <ServiceCard
+        showSnackbar={msg => console.info('snackbar', msg)}
+        preferredWorshipService="English"
+        cantoneseServices={cantoneseServices}
+        mandarinServices={mandarinServices}
+        englishServices={englishServices}
+      />
     </Container>
   );
 
