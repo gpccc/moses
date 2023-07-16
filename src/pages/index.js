241d976b1efd9b3eb887ea6fa2e3aafc6c3d49bb
Phu Vuong <pvuong@vuonglab.com>
2023-07-15 22:37:40 -0700

    Add worship services section.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 
 // import ChurchMapCard from "../components/church-map/church-map-card"
+import WorshipServices from "../views/worship-services"
 
 export default function Home() {
   return (
     <Layout>
+      <WorshipServices />
       {/* <ChurchMapCard /> */}
     </Layout>
  )
---
+++ src/views/worship-services.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+
+const WorshipServices = () => (
+    <Container component="section" sx={{ my: 4 }}>
+      <h1>Worship Services</h1>
+    </Container>
+  );
+
+export default WorshipServices
