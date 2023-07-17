014ac21cf02edc19b69f55bfeb75f8d1c755331d
Phu Vuong <pvuong@vuonglab.com>
2023-07-16 22:15:58 -0700

    Underline links in footer on hover only.

--- src/views/app-footer.js
+++ src/views/app-footer.js
@@ -13,7 +13,7 @@ const Copyright = () => (
   {'Copyright ©'}
   {new Date().getFullYear()}
   {' '}
-  <Link color="inherit" href="/">
+  <Link color="inherit" underline="hover" href="/">
   Greater Phoenix Chinese Christian Church
   </Link>
   </>
@@ -38,7 +38,7 @@ const AppFooter = () => (
     </Typography>
     </Box>
 
-    <Link target="_blank" rel="noreferrer" href="https://maps.google.com/?q=Greater Phoenix Chinese Christian Church, West Ray Road, Chandler, AZ" sx={{ color: 'white', pt: 2, pb: 1}}>
+    <Link target="_blank" rel="noreferrer" href="https://maps.google.com/?q=Greater Phoenix Chinese Christian Church, West Ray Road, Chandler, AZ" color="inherit" underline="hover" sx={{ pt: 2, pb: 1}}>
       <Box display="flex">
         <Box mr={1}><MapIcon fontSize="small" /></Box>
         Greater Phoenix Chinese Christian Church<br/>
@@ -47,21 +47,21 @@ const AppFooter = () => (
       </Box>
     </Link>
 
-    <Link href="tel:+1-480-786-4977" sx={{ color: 'white', pb: 1}}>
+    <Link href="tel:+1-480-786-4977" color="inherit" underline="hover" sx={{ pb: 1}}>
       <Box display="flex" alignItems="center">
         <Box mr={1}><PhoneIcon fontSize="small" /></Box>
         (480) 786-4977
       </Box>
     </Link>
 
-    <Link href="mailto:contact@gpccc.org" sx={{ color: 'white' }}>
+    <Link href="mailto:contact@gpccc.org" color="inherit" underline="hover" >
       <Box display="flex" alignItems="center">
         <Box mr={1}><EmailIcon fontSize="small" /></Box>
         contact@gpccc.org
       </Box>
     </Link>
 
-    <Typography variant="caption" sx={{ color: 'white', pt: 4 }}>
+    <Typography variant="caption" color="inherit" underline="hover" sx={{ pt: 4 }}>
       <Copyright />
     </Typography>
   </Typography>
