fb38b3015edacc19be9acb823af084e0ed0cc5f5
Phu Vuong <pvuong@vuonglab.com>
2023-07-18 23:44:01 -0700

    Delete unused import.

--- src/views/church-news.js
+++ src/views/church-news.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box'
 import Grid from '@mui/material/Unstable_Grid2'
 import Paper from '@mui/material/Paper'
 
