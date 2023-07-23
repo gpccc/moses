92bc37d02d6ca7fa6d018dcc98d29c2d5dbdfe02
Phu Vuong <pvuong@vuonglab.com>
2023-07-22 18:05:03 -0700

    Keep only last four most recent services.

--- src/constants/english-services.js
+++ src/constants/english-services.js
@@ -3,7 +3,6 @@ const englishServices = [
     { youtubeVideoID: "xt4UybrmZ3w", message: "The Yeast of These -- Matt. 16:1-28", pastor: "Pastor Chris Sung", date: "2023-07-16T16:30:00Z", duration: "01:23:35", seekPoints: [] },
     { youtubeVideoID: "nPbzum4abh8", message: "Did You Think That? — Matt. 15:21-39", pastor: "Pastor Larry Lee", date: "2023-07-09T16:30:00Z", duration: "01:18:40", seekPoints: [] },
     { youtubeVideoID: "IdcfWT-jyVI", message: "Did You Smell That? — Matt. 15:1-20", pastor: "Pastor Larry Lee", date: "2023-07-02T16:30:00Z", duration: "01:16:45", seekPoints: [] },
-    { youtubeVideoID: "k3fCavLUvKc", message: "A Tale of Two Kings — Matt. 14:1-21", pastor: "Pastor Chris Sung", date: "2023-06-25T16:30:00Z", duration: "01:10:40", seekPoints: [] },
 ]
 
 export default englishServices
