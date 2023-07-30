ffb5dd602a3170394c4e17226353b246ec187be6
Phu Vuong <pvuong@vuonglab.com>
2023-07-29 17:08:12 -0700

    Add 7/30/2023 English service.

--- src/constants/english-services.js
+++ src/constants/english-services.js
@@ -1,7 +1,8 @@
 import SeekPointType from './seek-point-type';
 
 const englishServices = [
-    { youtubeVideoID: "WAouZ0anz2I", message: "Death and Taxes -- Matt. 17:1-27", pastor: "Pastor Larry Lee", date: "2023-07-23T16:30:00Z", duration: "00:00:00", seekPoints: [] },
+    { youtubeVideoID: "iPP7dgcZh_4", message: "True Humility -- Matt. 18:1-35", pastor: "Pastor Larry Lee", date: "2023-07-30T16:30:00Z", duration: "00:00:00", seekPoints: [] },
+    { youtubeVideoID: "WAouZ0anz2I", message: "Death and Taxes -- Matt. 17:1-27", pastor: "Pastor Larry Lee", date: "2023-07-23T16:30:00Z", duration: "01:27:10", seekPoints: [] },
     { youtubeVideoID: "xt4UybrmZ3w", message: "The Yeast of These -- Matt. 16:1-28", pastor: "Pastor Chris Sung", date: "2023-07-16T16:30:00Z", duration: "01:23:35", seekPoints: [] },
     { youtubeVideoID: "nPbzum4abh8", message: "Did You Think That? — Matt. 15:21-39", pastor: "Pastor Larry Lee", date: "2023-07-09T16:30:00Z", duration: "01:18:40", seekPoints: [] },
     { youtubeVideoID: "IdcfWT-jyVI", message: "Did You Smell That? — Matt. 15:1-20", pastor: "Pastor Larry Lee", date: "2023-07-02T16:30:00Z", duration: "01:16:45", seekPoints: [] },
