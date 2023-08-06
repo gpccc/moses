5af77bf02abf1f957298ff4023c6632440c1947e
Phu Vuong <pvuong@vuonglab.com>
2023-08-05 22:30:38 -0700

    Add 8/6/2023 English service.

--- src/constants/english-services.js
+++ src/constants/english-services.js
@@ -1,11 +1,12 @@
 import SeekPointType from './seek-point-type';
 
 const englishServices = [
-    { youtubeVideoID: "iPP7dgcZh_4", message: "True Humility -- Matt. 18:1-35", pastor: "Pastor Larry Lee", date: "2023-07-30T16:30:00Z", duration: "00:00:00", seekPoints: [] },
+    { youtubeVideoID: "648IodvGKr8", message: "Being Faithful to Our Calling", pastor: "Bobby & Angie Cox", date: "2023-08-06T16:30:00Z", duration: "00:00:00", seekPoints: [] },
+
+    { youtubeVideoID: "iPP7dgcZh_4", message: "True Humility -- Matt. 18:1-35", pastor: "Pastor Larry Lee", date: "2023-07-30T16:30:00Z", duration: "01:22:01", seekPoints: [] },
     { youtubeVideoID: "WAouZ0anz2I", message: "Death and Taxes -- Matt. 17:1-27", pastor: "Pastor Larry Lee", date: "2023-07-23T16:30:00Z", duration: "01:27:10", seekPoints: [] },
     { youtubeVideoID: "xt4UybrmZ3w", message: "The Yeast of These -- Matt. 16:1-28", pastor: "Pastor Chris Sung", date: "2023-07-16T16:30:00Z", duration: "01:23:35", seekPoints: [] },
     { youtubeVideoID: "nPbzum4abh8", message: "Did You Think That? — Matt. 15:21-39", pastor: "Pastor Larry Lee", date: "2023-07-09T16:30:00Z", duration: "01:18:40", seekPoints: [] },
-    { youtubeVideoID: "IdcfWT-jyVI", message: "Did You Smell That? — Matt. 15:1-20", pastor: "Pastor Larry Lee", date: "2023-07-02T16:30:00Z", duration: "01:16:45", seekPoints: [] },
 
     { youtubeVideoID: "EWQ__H_85bE", message: "Rebuilding Opposed (Ezra 4)", pastor: "Rev. Larry Lee", date: "2020-10-04T16:30:00Z", duration: "01:25:45", seekPoints: [
         {time: "00:10:57", label: "Opening prayer", type: SeekPointType.PRAYER},
