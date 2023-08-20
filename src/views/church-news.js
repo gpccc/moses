c2bec194f02ca5f5a4971923e76bf0a969374bc9
Phu Vuong <pvuong@vuonglab.com>
2023-08-19 20:19:53 -0700

    Add 8/20/2023 church news

--- src/views/church-news.js
+++ src/views/church-news.js
@@ -19,9 +19,20 @@ const ChurchNews = () => {
 
   // Markdown syntax: https://commonmark.org/help/
   const churchNews = {
+    "2023-08-20": [
+      "The church is moving to **electronic voting** with the aim of improving voting efficiency and saving manpower. Voting tests will be open for the next two weeks. Mock ballots will be sent by email and SMS. The format is as follows:\n\n**Email from:** Greater Phoenix Chinese Christian Church <invitations@mail.electionbuddy.com>\n\nSubject: Vote now: Greater Phoenix Chinese Christian Church--Members Mock vote\n\n**Text (SMS) from:** (877) 225-8712\n\nGPCCC: Member Mock vote: VOTE Now - ebv.io/xxxx-xxxx-xxxx-xxxx",
+      "**2023 Mission Conference** is Fri.-Sun., Sep. 15-17. Our theme is \“Proclaim His Name\” and our speaker is Brent Gambrell (Brent Gambrell Ministries). 30 Day Prayer for Mission Conference begins Sat. Aug. 19.",
+      "**International Missions Potluck**, Sat., Sep. 16, 4:30–5:50pm. In the breezeway, grab a post-it with a food type on it and bring enough of your dish to feed five. This way, food from all our mission fields are represented.",
+      "**Prayer Meeting** is Wednesdays, 7:30-8:30pm (Zoom: 233 085 786; PW 007276). [Email P. Larry](mailto:larrylee@gpccc.org) for info.",
+      "**Patchwork Sisters** ministry will host a women's breakfast on Sat, Aug. 26, 10am-1pm. Theme: Coram Deo. Speakers: Maria Wong, Karen Palmer, and Robin Stanton. Registration deadline is Aug. 20. $5/person. To register, [email Amy Lee](mailto:amygmlee@yahoo.com).",
+      "**Crossroads Young Adults'** pool party is Sat., Aug. 26. [Email Eddie Kwan](mailto:edkwan88@hotmail.com) for info.",
+      "**College Bible Study** is on Wednesdays on ASU campus (exact location TBD) starting Aug. 23. If interested please [fill out the contact form](https://docs.google.com/forms/d/e/1FAIpQLScz1aqYNTb7BXT62nuv6RokpNvRmFk9MXbvEzAT9v0hJVf0wA/viewform?pli=1). Please [email P. Chris](mailto:chris.sung@gpccc.org) for more information.",
+    ],
+
+
     "2023-08-13": [
       "The church is moving to **electronic voting** with the aim of improving voting efficiency and saving manpower. Voting tests will be open for the next two weeks. Mock ballots will be sent by email and SMS. The format is as follows:\n\n**Email from:** Greater Phoenix Chinese Christian Church <invitations@mail.electionbuddy.com>\n\nSubject: Vote now: Greater Phoenix Chinese Christian Church--Members Mock vote\n\n**Text (SMS) from:** (877) 225-8712\n\nGPCCC: Member Mock vote: VOTE Now - ebv.io/xxxx-xxxx-xxxx-xxxx",
-      "**2023 Mission Conference** is Fri.-Sun., Sep. 15-17. Our theme is \“Proclaim His Name\” and our speaker is Brent Gambrell (Brent Gambrell Ministries). 30 Day Prayer for Mission Conference begins Sat. Aug, 19.",
+      "**2023 Mission Conference** is Fri.-Sun., Sep. 15-17. Our theme is \“Proclaim His Name\” and our speaker is Brent Gambrell (Brent Gambrell Ministries). 30 Day Prayer for Mission Conference begins Sat. Aug. 19.",
       "**College Summer Hangout** Sundays, 6pm for dinner, *The Compelling Community* book study, and hangout. [Schedule and book PDF](https://docs.google.com/spreadsheets/u/0/d/1LBt5VU7scEmssBQvSBKXA077QnPAIBGmUT1GqgXZid8/htmlview#gid=0). [Email Keith](mailto:english.college@gpccc.org) for info.",
       "**Prayer Meeting** is Wednesdays, 7:30-8:30pm (Zoom: 233 085 786; PW 007276). [Email P. Larry](mailto:larrylee@gpccc.org) for info.",
       "**Reader's Circle** is Sat., Aug. 19, 10am at Phu & Henna's. They are reading _Freedom of Simplicity_ by Richard Foster. [Email Sheng Peng](mailto:sypeng123@yahoo.com) for info.",
