5e81a5effc55d87b8660b125cc8d36e8ba60c90b
Phu Vuong <pvuong@vuonglab.com>
2023-09-14 22:40:03 -0700

    Configure gatsby-plugin-react-i18next.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,50 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`
 })
 
+const {languages, defaultLanguage} = require('./languages')
+const siteUrl = process.env.URL || 'https://gpccc.netlify.app/'
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/locales`,
+        name: 'locale'
+      }
+    },
+    {
+      resolve: '@herob/gatsby-plugin-react-i18next',
+      options: {
+        localeJsonSourceName: 'locale', // name given to 'gatsby-source-filesystem' plugin.
+        languages,
+        defaultLanguage,
+        siteUrl,
+        // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
+        trailingSlash: 'always',
+        i18nextOptions: {
+          defaultNS: 'common',
+          debug: true,
+          interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+        pages: [
+          /*
+          {
+            matchPath: '/:lang?/blog/:uid',
+            getLanguageFromPath: true,
+            excludeLanguages: ['es'],
+          },
+          {
+            matchPath: '/ignored-page',
+            languages: ['en'],
+          }
+          */
+        ]
+      }
+    }   
+  ],
 }
---
+++ languages.js
@@ -0,0 +1,4 @@
+module.exports = {
+  languages: ['zh', 'zf', 'en'],
+  defaultLanguage: 'en',
+}
