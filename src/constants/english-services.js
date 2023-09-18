d6f8b3683441fb96e3aabfd985c4cdd697acfe89
Phu Vuong <pvuong@vuonglab.com>
2023-09-17 21:29:20 -0700

    Add most recent four English services.

--- src/constants/english-services.js
+++ src/constants/english-services.js
@@ -1,8 +1,11 @@
 import SeekPointType from './seek-point-type';
 
 const englishServices = [
-    { youtubeVideoID: "jNUO0Q6tGKc", message: "Last and First -- Matt. 20:1-34", pastor: "Pastor Larry Lee", date: "2023-08-20T16:30:00Z", duration: "00:00:00", seekPoints: [] },
-
+    { youtubeVideoID: "NSWjZdNCdGE", message: "Redeeming the Time — Ephesians 5:14-21", pastor: "Brent Gambrell", date: "2023-09-17T16:30:00Z", duration: "01:21:35", seekPoints: [] },
+    { youtubeVideoID: "WMgSQBo5YY8", message: "The Great Feast — Matt. 22:1-22", pastor: "Pastor Larry Lee", date: "2023-09-10T16:30:00Z", duration: "01:19:15", seekPoints: [] },
+    { youtubeVideoID: "oUgwSCi84wE", message: "Answering in Parables — Matt. 21:23-46", pastor: "Pastor Larry Lee", date: "2023-09-03T16:30:00Z", duration: "01:23:40", seekPoints: [] },
+    { youtubeVideoID: "W2DrUImhVYI", message: "Judgement on Jerusalem — Matt. 21:12-22", pastor: "Pastor Chris Sung", date: "2023-08-27T16:30:00Z", duration: "01:07:05", seekPoints: [] },
+    { youtubeVideoID: "jNUO0Q6tGKc", message: "Last and First -- Matt. 20:1-34", pastor: "Pastor Larry Lee", date: "2023-08-20T16:30:00Z", duration: "01:18:10", seekPoints: [] },
     { youtubeVideoID: "u_X8le7mFi4", message: "Broken -- Matt. 19:1-30", pastor: "Pastor Larry Lee", date: "2023-08-13T16:30:00Z", duration: "01:28:30", seekPoints: [] },
     { youtubeVideoID: "648IodvGKr8", message: "Being Faithful to Our Calling", pastor: "Bobby & Angie Cox", date: "2023-08-06T16:30:00Z", duration: "01:20:55", seekPoints: [] },
     { youtubeVideoID: "iPP7dgcZh_4", message: "True Humility -- Matt. 18:1-35", pastor: "Pastor Larry Lee", date: "2023-07-30T16:30:00Z", duration: "01:22:01", seekPoints: [] },
