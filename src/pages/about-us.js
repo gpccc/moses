71ef2099ae565f526dbe5f6036be94be33182c44
Phu Vuong <pvuong@vuonglab.com>
2023-09-18 23:43:03 -0700

    Display about-us page in English or Mandarin.

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,13 +1,34 @@
 import React from "react"
 
+import {graphql} from 'gatsby'
+
 import Layout from "../components/layout"
 
+import { useI18next } from '@herob/gatsby-plugin-react-i18next'
+
 import OurBeliefPurposeEnglish from "../views/belief-purpose-english"
+import OurBeliefPurposeMandarin from "../views/belief-purpose-mandarin"
 
 export default function AboutUs() {
+  const { i18n } = useI18next()
+
   return (
     <Layout>
-      <OurBeliefPurposeEnglish />
+      { i18n.language==="en" ? <OurBeliefPurposeEnglish /> : <OurBeliefPurposeMandarin /> }
     </Layout>
  )
 }
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: {ns: {in: ["common"]}, language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`
