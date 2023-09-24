8eb3134f17383982135eef40d928028b5a757611
Phu Vuong <pvuong@vuonglab.com>
2023-09-23 19:43:12 -0700

    Internationalize 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,35 @@
 import React from "react"
+import {graphql} from 'gatsby'
 import Button from "@mui/material/Button"
 
 import Layout from "../components/layout"
 
+import {useI18next} from '@herob/gatsby-plugin-react-i18next'
+
 export default function PageNotFound(props) {
   const url = props.location.pathname
 
+  const { t } = useI18next()
+
   return (
     <Layout>
-        <h3>Error 404: Page not found</h3>
-        <p>The requested link {url} is broken.</p>
-        <Button onClick={() => props.navigate(-1)}>Go back</Button>
+        <h3>{t("Error 404: Page not found")}</h3>
+        <p>{t("The_requested_link_url_is_broken", {url})}</p>
+        <Button onClick={() => props.navigate(-1)}>{t("Go back")}</Button>
     </Layout>
  )
 }
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: {ns: {in: ["common"]}, language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`
