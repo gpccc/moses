31548cb01985cbbc82210a7501b260096ed53d89
Phu Vuong <pvuong@vuonglab.com>
2023-09-24 21:40:14 -0700

    Add broken bread to 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,8 @@ import Layout from "../components/layout"
 
 import {useI18next} from '@herob/gatsby-plugin-react-i18next'
 
+import {BrokenBread} from "../components/broken-bread"
+
 export default function PageNotFound(props) {
   const url = props.location.pathname
 
@@ -16,6 +18,8 @@ export default function PageNotFound(props) {
         <h3>{t("Error 404: Page not found")}</h3>
         <p>{t("The_requested_link_url_is_broken", {url})}</p>
         <Button onClick={() => navigate(-1)}>{t("Go back")}</Button>
+
+        <BrokenBread />
     </Layout>
  )
 }
