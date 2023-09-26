2b26217905c2c0c6372de79d1330f5218a6686c0
Phu Vuong <pvuong@vuonglab.com>
2023-09-25 22:24:06 -0700

    Center 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {navigate, graphql} from 'gatsby'
-import Button from "@mui/material/Button"
+import {Box, Button, Typography} from "@mui/material/"
 
 import Layout from "../components/layout"
 
@@ -15,11 +15,12 @@ export default function PageNotFound(props) {
 
   return (
     <Layout showCongregationTabs={false}>
-        <h3>{t("Error 404: Page not found")}</h3>
-        <p>{t("The_requested_link_url_is_broken", {url})}</p>
-        <Button onClick={() => navigate(-1)}>{t("Go back")}</Button>
-
+      <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" sx={{mx: 2, my: 1}}>
+        <Typography variant="h3">{t("Page not found")}</Typography>
+        <Typography variant="h5" sx={{mt: 2}}>{t("The_requested_link_url_is_broken", {url})}</Typography>
         <BrokenBread />
+        <Button variant="contained" disableElevation sx={{mt: 1}} onClick={() => navigate(-1)}>{t("Go back")}</Button>
+      </Box>
     </Layout>
  )
 }
